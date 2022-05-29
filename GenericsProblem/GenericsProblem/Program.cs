c7b27f15a590c4440236ae5c88f373e23377f9b1
prashantmagare <prashantmagare3010@gmail.com>
2022-05-29 23:41:12 +0530

    "Added - UC3_MaxStrings"

--- GenericsProblem/GenericsProblem/Program.cs
+++ GenericsProblem/GenericsProblem/Program.cs
@@ -6,14 +6,10 @@ namespace Max_Generics
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("UseCase 1 Find Max Int  Number");
+            Console.WriteLine("UseCase 3 Find Max String  Number");
 
-            //int intOutput = MaxNumberCheck_Double.MaxNumbermethod_Int(11, 22, 33);
-            //Console.Write(intOutput);
-            Console.WriteLine("UseCase 2 Find Max Double  Number");
-
-            double doubleOutput = MaxNumberCheck_Double.MaxNumbermethod_Double(112.3, 22.23, 33.6);
-            Console.Write(doubleOutput);
+            string stringOutput = MaxNumberCheck_String.MaxNumbermethod_String("Apple", "Banana", "Peach");
+            Console.Write(stringOutput);
         }
     }
 }
