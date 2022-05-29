14ab5b87a4e684c6b6ce8d60a69cd40fa610b854
prashantmagare <prashantmagare3010@gmail.com>
2022-05-29 23:37:59 +0530

    "Added - UC2_MaxProblem"

--- GenericsProblem/GenericsProblem/Program.cs
+++ GenericsProblem/GenericsProblem/Program.cs
@@ -6,10 +6,14 @@ namespace Max_Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("UseCase 1 Find Max Int  Number");
+            //Console.WriteLine("UseCase 1 Find Max Int  Number");
 
-            int intOutput = MaxNumberCheck.MaxNumbermethod_Int(11, 22, 33);
-            Console.Write(intOutput);
+            //int intOutput = MaxNumberCheck_Double.MaxNumbermethod_Int(11, 22, 33);
+            //Console.Write(intOutput);
+            Console.WriteLine("UseCase 2 Find Max Double  Number");
+
+            double doubleOutput = MaxNumberCheck_Double.MaxNumbermethod_Double(112.3, 22.23, 33.6);
+            Console.Write(doubleOutput);
         }
     }
 }
