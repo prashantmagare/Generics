110bd24755e50d64e4648eea846356079553a305
prashantmagare <prashantmagare3010@gmail.com>
2022-05-29 23:34:46 +0530

    "Added - UC1_MaxNumber"

--- GenericsProblem/GenericsProblem/Program.cs
+++ GenericsProblem/GenericsProblem/Program.cs
@@ -6,14 +6,10 @@ namespace Max_Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Find Max Int  Number using Generic Method");
-            Refactor.FindMax<int>(11, 22, 9);
+            Console.WriteLine("UseCase 1 Find Max Int  Number");
 
-            Console.WriteLine("Find Max Double  Number using Generic Method");
-            Refactor.FindMax<double>(11.3, 22.23, 9.55);
-
-            Console.WriteLine("Find Max String Number using Generic Method");
-            Refactor.FindMax<string>("Apple", "Peach", "Banana");
+            int intOutput = MaxNumberCheck.MaxNumbermethod_Int(11, 22, 33);
+            Console.Write(intOutput);
         }
     }
 }
